28c69edec1b37b8bac7769c8642535863fbf1487
Iv-0-vI <132075743+Iv-0-vI@users.noreply.github.com>
2024-10-15 16:33:50 +0800

    python

---
+++ Java/Project/Java/Main.java
@@ -0,0 +1,28 @@
+//Java 我能为你做些什么
+public class Main {
+    public static void main(String[] args) {
+        printHello();// print: Hello Lzk
+        System.out.println("============================================");
+        String name = "Java";
+        bianliJava(name);// output Java in line
+        System.out.println("============================================");
+        System.out.println('J' + 'a' + 'v' + 'a');// assemble Java
+        System.out.println("============================================");
+    }
+
+    public static void bianliJava(String name) {
+
+        for (int i = 0; i < 4; i++) {
+            System.out.println(name.charAt(i));
+        }
+    }
+
+    public static void printHello() {
+        System.out.println("Hello Lzk");
+    }
+}
+
+class Java {
+    private String name = "Java";
+    private int age = 22;
+}
