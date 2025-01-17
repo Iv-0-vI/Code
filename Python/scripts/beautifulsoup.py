17b84a907c07d3fb2239e843691415de512d4577
Iv-0-vI <132075743+Iv-0-vI@users.noreply.github.com>
2025-01-17 15:16:58 +0800

    更新 beautifulsoup.py

--- Python/scripts/beautifulsoup.py
+++ Python/scripts/beautifulsoup.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+# 将一大块东西命名
 response = requests.get('https://www.runoob.com')
 soup = BeautifulSoup(response.text, 'html.parser')
 title = soup.find('title').text
